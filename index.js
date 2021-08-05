9da55a83d2bd071aee82afc3c11e71ba81fba210
github username <um_2003_8@hotmail.com>
2021-08-05 13:24:36 -0400

    create walls variable that creates rectangels and add it to World which will display borders around the canvas

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { Engine, Render, Runner, World, Bodies } = Matter 
 
+const width = 600
+const height = 600
+
 const engine = Engine.create()
 const { world } = engine
 const render = Render.create({
@@ -15,4 +18,11 @@ const render = Render.create({
 Render.run(render)
 Runner.run(Runner.create(), engine)
 
+const walls = [
+    Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+    Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+    Bodies.rectangle(0, height / 2,40, height, { isStatic: true }),
+    Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+]
 
+World.add(world, walls)
