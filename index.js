ea9805c35d6f558bdda31154b61dfc57c7028f80
github username <um_2003_8@hotmail.com>
2021-08-05 13:20:11 -0400

    create render that give option of canvas size

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const { Engine, Render, Runner, World, Bodies } = Matter
 
 const engine = Engine.create()
 const { world } = engine
+const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+        wireframes: false,
+        width,
+        height
+    }
+})
+
+Render.run(render)
 
